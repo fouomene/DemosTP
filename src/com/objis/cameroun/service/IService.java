dc32aabddae1680b25e8320c7213f9d7767fff37
fouomene <fouomene@bitbucket.org>
2018-05-23 17:30:40 +0100

    TP TEST

---
+++ src/com/objis/cameroun/service/IService.java
@@ -0,0 +1,37 @@
+package com.objis.cameroun.service;
+
+public interface IService {
+	
+	//sera implementée par Cyrille
+	public int somme(int a, int b);
+	
+	//sera implementée par Frank
+	public int soustration(int a, int b);
+	
+	//sera implementée par Emmanuel
+	public int division(int a, int b);
+	
+	//sera implementée par Maurain
+	public int produit(int a, int b);
+	
+	//sera implementée par Sonia
+	public int perimetreRectangle(int longeur, int largeur);
+	
+	//sera implementée par Flaubert
+	public int surfaceRectangle(int longeur, int largeur);
+	
+	//sera implementée par Axel
+	public int conversionFCFAEURO(int montant);
+	
+	//sera implementée par Flaubert
+	public int perimetreCarre(int cote);
+	
+	//sera implementée par Stephanie
+	public int calculBenefice(int prixAchat, int prixVente);
+	
+	
+	
+	
+	
+
+}
---
+++ src/com/objis/cameroun/service/Service.java
@@ -0,0 +1,63 @@
+package com.objis.cameroun.service;
+
+public class Service implements IService {
+
+	public Service() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int somme(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int soustration(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int division(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int produit(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int perimetreRectangle(int longeur, int largeur) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int surfaceRectangle(int longeur, int largeur) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int conversionFCFAEURO(int montant) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int perimetreCarre(int cote) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int calculBenefice(int prixAchat, int prixVente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
